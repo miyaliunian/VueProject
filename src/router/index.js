8b2ac402fd953420b0290d46cc7d541dcf578260
米亚流年 <354849262@qq.com>
2021-03-03 10:41:02 +0800

    bug修改

--- src/router/index.js
+++ src/router/index.js
@@ -100,6 +100,12 @@ const routes = [
   },
 ];
 
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch((err) => err);
+};
+
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
